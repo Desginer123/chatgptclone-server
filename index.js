c2c976e677659459b2e1a1c8a153f5acfb7b205d
Vladimir <lisoff.vol@yandex.ru>
2023-02-18 19:21:16 +0300

    test

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
 import {Configuration, OpenAIApi} from "openai";
+const apikey = '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'
 const configuration = new Configuration({
     organization: "org-GbLMg4RhsXc3ClODecgJIKdn",
-    apiKey: "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",
+    apiKey: apikey,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -22,6 +23,11 @@ app.post("/", async (req, res) => {
             prompt: `${message}`,
             max_tokens: 150,
             temperature: 0.8,
+        },
+        {
+            headers: {
+                'Authorization': 'Bearer ' + apikey
+            }
         });
         res.json({message: aiResponce.data.choices[0].text});
     } catch (error) {
